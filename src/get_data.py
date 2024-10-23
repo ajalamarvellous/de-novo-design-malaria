3465b056dd5b87ea85fd7a41e2ceba7cc520c97a
shelby <ajalaoluwamayowa00@gmail.com>
2024-10-24 00:30:07 +0100

    Delete funcs moved to src/utils

--- src/get_data.py
+++ src/get_data.py
@@ -7,32 +7,13 @@ from typing import List, TypeVar
 import pandas as pd
 import argparse
 
-# defining new variable for our artifact locations
-addressType = TypeVar("addressType", str, Path)
+
 # basic logging config
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s %(funcName)s[%(levelname)s]: %(message)s ",
 )
 logger = logging.getLogger()
-
-def read_file(file_address: addressType) -> pd.DataFrame:
-    """
-    Read the data file in pandas and return a pandas dataframe
-
-    Argument(s)
-    ------------
-    file_address: str \n
-        address of the file to be opened
-
-    Return(s)
-    -----------
-    file_dataframe: pd.DataFrame \n
-        pandas dataframe of the opened file
-    """
-    df = pd.read_table(file_address, low_memory=False)
-    logger.info("File read successfully...")
-    return df
 
 
 def describe_data(df: pd.DataFrame) -> None:
