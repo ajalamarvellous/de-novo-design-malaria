c0754092043e0ae7391445d3534e7290d3e57835
shelby <ajalaoluwamayowa00@gmail.com>
2024-10-24 00:19:48 +0100

    Fix bug on read file to allow the specification of whether csv or not

--- src/utils.py
+++ src/utils.py
@@ -5,16 +5,16 @@ from rdkit.Chem import rdFingerprintGenerator
 import pandas as pd
 import numpy as np
 
-from typing import TypeVar, List, Union
+from typing import TypeVar, List, Union, NewType
 from pathlib import Path
 
 
 # defining new variable for our artifact locations
 addressType = TypeVar("addressType", str, Path)
-molType = TypeVar("molType", Chem.rdchem.Mol)
-fngrprntType = TypeVar("fngrprntType", cDataStructs.ExplicitBitVect)
+molType = NewType("molType", Chem.rdchem.Mol)
+fngrprntType = NewType("fngrprntType", cDataStructs.ExplicitBitVect)
 
-def read_file(file_address: addressType) -> pd.DataFrame:
+def read_file(file_address: addressType, dtype="csv") -> pd.DataFrame:
     """
     Read the data file in pandas and return a pandas dataframe
 
@@ -22,16 +22,22 @@ def read_file(file_address: addressType) -> pd.DataFrame:
     ------------
     file_address: str \n
         address of the file to be opened
+    
+    dtype: str \n
+        data type, whether csv or table
 
     Return(s)
     -----------
     file_dataframe: pd.DataFrame \n
         pandas dataframe of the opened file
     """
-    df = pd.read_table(file_address, low_memory=False)
+    if dtype != "table":
+        df = pd.read_csv(file_address, low_memory=False)
+    else:
+        df = pd.read_table(file_address, low_memory=False)
     return df
 
-def get_mols(df: pd.DataFrame, column: str) -> List[molType]:
+def get_mols(df: pd.DataFrame, column: str="SMILES") -> List[molType]:
     """
     Return the MOL for the smiles given
 
@@ -82,7 +88,7 @@ def get_fingerprints(
         radius=radius, fpSize=fp_size
         )
     if as_numpy:
-        fingerprints = np.array([fingerprnt_gen.GetFingerprintsAsNumpy(x) for x in mols])
+        fingerprints = np.array([fingerprnt_gen.GetFingerprintAsNumPy(x) for x in mols])
     else:
         fingerprints = fingerprnt_gen.GetFingerprints(mols)
     return fingerprints
