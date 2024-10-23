d27072ddb319a1b43b6c08b371ffd1fafa08387c
shelby <ajalaoluwamayowa00@gmail.com>
2024-10-23 22:42:35 +0100

    Add a fingerprint_generator func

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,18 @@
 from rdkit import Chem
+from rdkit.DataStructs import cDataStructs
+from rdkit.Chem import rdFingerprintGenerator
 
 import pandas as pd
 import numpy as np
 
-from typing import TypeVar
+from typing import TypeVar, List, Union
 from pathlib import Path
 
 
 # defining new variable for our artifact locations
 addressType = TypeVar("addressType", str, Path)
+molType = TypeVar("molType", Chem.rdchem.Mol)
+fngrprntType = TypeVar("fngrprntType", cDataStructs.ExplicitBitVect)
 
 def read_file(file_address: addressType) -> pd.DataFrame:
     """
@@ -27,7 +31,7 @@ def read_file(file_address: addressType) -> pd.DataFrame:
     df = pd.read_table(file_address, low_memory=False)
     return df
 
-def get_mols(df: pd.DataFrame, column: str) -> list:
+def get_mols(df: pd.DataFrame, column: str) -> List[molType]:
     """
     Return the MOL for the smiles given
 
@@ -46,3 +50,40 @@ def get_mols(df: pd.DataFrame, column: str) -> list:
     """
     mols = [Chem.MolFromSmiles(x) for x in df[column]]
     return mols
+
+
+def get_fingerprints(
+        mols: List[molType], radius: int=2, fp_size: int= 1028, as_numpy: bool=True
+        ) -> Union[np.array, fngrprntType]:
+    """
+    Return morgan Fingerprints of the MOL supplied
+
+    Argument(s)
+    ------------
+    mols: List[molType] \n
+        List of mols supplied
+    
+    radius: int \n
+        parameter for the fingerprint generator to specify the radius for the 
+        fingerprint to be returned
+    
+    fp_size: int \n
+        len of the fingerprint to be returned
+
+    as_numpy: bool \n
+        whether to return the fingerprints as numpy
+
+    Return(s)
+    -----------
+    fingerprints: np.array \n
+        array of the fingerprints for the rdkit MOLs supplied
+    """
+    fingerprnt_gen = rdFingerprintGenerator.GetMorganGenerator(
+        radius=radius, fpSize=fp_size
+        )
+    if as_numpy:
+        fingerprints = np.array([fingerprnt_gen.GetFingerprintsAsNumpy(x) for x in mols])
+    else:
+        fingerprints = fingerprnt_gen.GetFingerprints(mols)
+    return fingerprints
+    
